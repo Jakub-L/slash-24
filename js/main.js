fe53152b43ccb73ddba9676673c029a72ef23b63
Jakub L <47220449+Jakub-L@users.noreply.github.com>
2022-07-30 19:10:58 +0100

    Add basic table display

--- js/main.js
+++ js/main.js
@@ -7,36 +7,73 @@ const maskLengthInput = document.getElementById('mask-length-input');
 const maskLengthSlider = document.getElementById('mask-length-slider');
 const ipAddressInput = document.getElementById('ip-address-input');
 const ipAddressValidator = document.getElementById('ip-address-validator');
+const ipAddressTable = document.getElementById('ip-address-table');
 
 // INITIALISATION
 maskLengthInput.value = INITIAL_BITMASK_LENGTH;
 maskLengthSlider.value = INITIAL_BITMASK_LENGTH;
 ipAddressInput.value = INITIAL_IP_ADDRESS;
-let isIpValid = true;
+let octets;
+let binOctets;
+let maskBinOctets;
+setMaskOctets(INITIAL_BITMASK_LENGTH);
+setIpOctets(parseIp(INITIAL_IP_ADDRESS));
+generateIpTable();
 
 // EVENT LISTENERS
 maskLengthInput.addEventListener('change', bitmaskLengthChange);
 maskLengthSlider.addEventListener('change', bitmaskLengthChange);
-ipAddressInput.addEventListener('change', checkIpValidity);
+ipAddressInput.addEventListener('change', ipAddressChange);
 
 // EVENT HANDLERS
 function bitmaskLengthChange({ target }) {
   const clampedValue = Math.max(0, Math.min(32, target.value));
   maskLengthInput.value = clampedValue;
   maskLengthSlider.value = clampedValue;
+  setMaskOctets(clampedValue);
+  generateIpTable();
 }
 
-function checkIpValidity({ target }) {
-  const octets = getOctets(target.value);
-  isIpValid = octets.length === 4 && octets.every(octet => octet >= 0 && octet <= 255);
+function ipAddressChange({ target }) {
+  const newOctets = parseIp(target.value);
+  const isIpValid =
+    newOctets.length === 4 && newOctets.every(octet => octet >= 0 && octet <= 255);
   if (isIpValid) {
+    setIpOctets(newOctets);
     ipAddressValidator.classList.add('hidden');
   } else {
     ipAddressValidator.classList.remove('hidden');
   }
+  generateIpTable();
 }
 
 // UTILS
-function getOctets(str) {
+function generateIpTable() {
+  function generateRow(label, values) {
+    let row = ipAddressTable.insertRow();
+    let labelCell = row.insertCell(0);
+    labelCell.innerHTML = label;
+    values.forEach((value, i) => {
+      const cell = row.insertCell(i + 1);
+      cell.innerHTML = value;
+    });
+  }
+
+  ipAddressTable.innerHTML = '';
+  generateRow('IP address (decimal)', octets);
+  generateRow('IP address (binary)', binOctets);
+  generateRow('Network mask (binary)', maskBinOctets);
+}
+
+function parseIp(str) {
   return str.split('.').map(Number);
+}
+
+function setIpOctets(newOctets) {
+  octets = newOctets;
+  binOctets = newOctets.map(o => o.toString(2).padStart(8, 0));
+}
+
+function setMaskOctets(maskLength) {
+  maskBinOctets = '1'.repeat(maskLength).padEnd(32, 0).match(/.{8}/g);
 }
