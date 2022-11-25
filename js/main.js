e95af988aee8b3cc8b11cecd49817d93f3a07e92
Jakub L <47220449+Jakub-L@users.noreply.github.com>
2022-11-25 21:53:34 +0000

    Simplify JS to work with pared back HTML

--- js/main.js
+++ js/main.js
@@ -3,14 +3,10 @@ const INITIAL_BITMASK_LENGTH = 24;
 const INITIAL_IP_ADDRESS = '172.16.254.149';
 
 // ELEMENTS
-const maskLengthInput = document.getElementById('mask-length-input');
-const maskLengthSlider = document.getElementById('mask-length-slider');
-const maskTable = document.getElementById('mask-table');
-const ipAddressInput = document.getElementById('ip-address-input');
-const ipAddressValidator = document.getElementById('ip-address-validator');
-const ipAddressTable = document.getElementById('ip-address-table');
-const networkPrefixTable = document.getElementById('network-prefix-table');
-const hostPartTable = document.getElementById('host-part-table');
+const maskLengthInput = document.getElementById('mask__input');
+const maskLengthSlider = document.getElementById('mask__slider');
+const ipAddressInput = document.getElementById('ip__input');
+const ipAddressValidator = document.getElementById('ip__validator');
 
 // INITIALISATION
 maskLengthInput.value = INITIAL_BITMASK_LENGTH;
@@ -23,10 +19,6 @@ let maskBinOctets;
 let maskComplementBinOctets;
 setMask(INITIAL_BITMASK_LENGTH);
 setIpOctets(parseIp(INITIAL_IP_ADDRESS));
-generateMaskTable();
-generateIpTable();
-generateNetworkPrefixTable();
-generateHostPartTable();
 
 // EVENT LISTENERS
 maskLengthInput.addEventListener('change', bitmaskLengthChange);
@@ -39,9 +31,6 @@ function bitmaskLengthChange({ target }) {
   maskLengthInput.value = clampedValue;
   maskLengthSlider.value = clampedValue;
   setMask(clampedValue);
-  generateMaskTable();
-  generateNetworkPrefixTable();
-  generateHostPartTable();
 }
 
 function ipAddressChange({ target }) {
@@ -52,65 +41,6 @@ function ipAddressChange({ target }) {
     ipAddressValidator.classList.add('hidden');
   } else {
     ipAddressValidator.classList.remove('hidden');
-  }
-  generateIpTable();
-  generateNetworkPrefixTable();
-  generateHostPartTable();
-}
-
-// TABLE UTIL FUNCTIONS
-function generateRow(table, label, values) {
-  const row = table.insertRow();
-  [label, ...values].forEach((value, i) => {
-    const cell = row.insertCell(i);
-    cell.innerHTML = value;
-  });
-}
-
-function generateIpTable() {
-  ipAddressTable.innerHTML = '';
-  generateRow(ipAddressTable, 'IP address (decimal)', ipOctets);
-  generateRow(ipAddressTable, 'IP address (binary)', ipBinOctets);
-}
-
-function generateMaskTable() {
-  maskTable.innerHTML = '';
-  generateRow(maskTable, 'Network mask (binary)', maskBinOctets);
-}
-
-function generateNetworkPrefixTable() {
-  networkPrefixTable.innerHTML = '';
-  const ipArray = ipBinOctets.join('').split('');
-  const maskArray = maskBinOctets.join('').split('');
-  const prefixBinaryArray = ipArray.map((bit, i) => bit === '.' ? '.' : Number(bit) & Number(maskArray[i]));
-  generateRow(networkPrefixTable, 'IP Address', ipArray);
-  generateRow(networkPrefixTable, 'Mask', maskArray);
-  generateRow(networkPrefixTable, 'Network prefix', prefixBinaryArray);
-
-  for (let i = 0; i < networkPrefixTable.rows.length; i++) {
-    const row = networkPrefixTable.rows[i];
-    for (let j = 1; j < row.cells.length; j++) {
-      if (j <= maskBitLength) row.cells[j].classList.add('text-red');
-      else row.cells[j].classList.add('text-grey');
-    }
-  }
-}
-
-function generateHostPartTable() {
-  hostPartTable.innerHTML = '';
-  const ipArray = ipBinOctets.join('').split('');
-  const maskComplement = maskComplementBinOctets.join('').split('');
-  const hostPartBinArray = ipArray.map((bit, i) => bit === '.' ? '.' : Number(bit) & Number(maskComplement[i]));
-  generateRow(hostPartTable, 'IP Address', ipArray);
-  generateRow(hostPartTable, 'Mask complement', maskComplement);
-  generateRow(hostPartTable, 'Host part', hostPartBinArray);
-
-  for (let i = 0; i < hostPartTable.rows.length; i++) {
-    const row = hostPartTable.rows[i];
-    for (let j = 1; j < row.cells.length; j++) {
-      if (j > maskBitLength) row.cells[j].classList.add('text-red');
-      else row.cells[j].classList.add('text-grey');
-    }
   }
 }
 
