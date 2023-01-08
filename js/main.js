f8a9a6ffc89dd793fa4ce7a3b424b0907962e10c
Jakub L <47220449+Jakub-L@users.noreply.github.com>
2023-01-08 23:02:56 +0000

    Add binary representation of IP and mask. Tidy up CSS names

--- js/main.js
+++ js/main.js
@@ -3,22 +3,21 @@ const INITIAL_BITMASK_LENGTH = 24;
 const INITIAL_IP_ADDRESS = '172.16.254.149';
 
 // ELEMENTS
-const maskLengthInput = document.getElementById('mask__input');
-const maskLengthSlider = document.getElementById('mask__slider');
-const ipAddressInput = document.getElementById('ip__input');
-const ipAddressValidator = document.getElementById('ip__validator');
+const ipAddressInput = document.getElementById('ip_input__field');
+const ipAddressValidator = document.getElementById('ip_input__validator');
+const ipDecimalOctets = document.querySelectorAll('[data-conversion=ip-decimal]');
+const ipBinaryOctets = document.querySelectorAll('[data-conversion=ip-binary]');
+
+const maskLengthInput = document.getElementById('mask_input__field');
+const maskLengthSlider = document.getElementById('mask_input__slider');
+const maskDecimalOctets = document.querySelectorAll('[data-conversion=mask-decimal]');
+const maskBinaryOctets = document.querySelectorAll('[data-conversion=mask-binary]');
 
 // INITIALISATION
-maskLengthInput.value = INITIAL_BITMASK_LENGTH;
-maskLengthSlider.value = INITIAL_BITMASK_LENGTH;
-ipAddressInput.value = INITIAL_IP_ADDRESS;
-let maskBitLength = INITIAL_BITMASK_LENGTH;
-let ipOctets;
-let ipBinOctets;
-let maskBinOctets;
-let maskComplementBinOctets;
+setIp(INITIAL_IP_ADDRESS);
 setMask(INITIAL_BITMASK_LENGTH);
-setIpOctets(parseIp(INITIAL_IP_ADDRESS));
+setMaskOctets(maskToOctets(INITIAL_BITMASK_LENGTH));
+setIpOctets(ipToOctets(INITIAL_IP_ADDRESS));
 
 // EVENT LISTENERS
 maskLengthInput.addEventListener('change', bitmaskLengthChange);
@@ -28,15 +27,15 @@ ipAddressInput.addEventListener('change', ipAddressChange);
 // EVENT HANDLERS
 function bitmaskLengthChange({ target }) {
   const clampedValue = Math.max(0, Math.min(32, target.value));
-  maskLengthInput.value = clampedValue;
-  maskLengthSlider.value = clampedValue;
   setMask(clampedValue);
+  setMaskOctets(maskToOctets(clampedValue));
 }
 
 function ipAddressChange({ target }) {
-  const newOctets = parseIp(target.value);
-  const isIpValid = newOctets.length === 4 && newOctets.every(octet => octet >= 0 && octet <= 255);
-  if (isIpValid) {
+  const newIp = target.value;
+  const newOctets = ipToOctets(newIp);
+  if (isValidIp(newOctets)) {
+    setIp(newIp);
     setIpOctets(newOctets);
     ipAddressValidator.classList.add('hidden');
   } else {
@@ -45,18 +44,46 @@ function ipAddressChange({ target }) {
 }
 
 // GENERAL UTILS
-function parseIp(str) {
-  return str.split('.').map(Number);
+function ipToOctets(ip) {
+  return ip.split('.').map(Number);
+}
+
+function maskToOctets(length) {
+  return '1'.repeat(length).padStart(32, '0').match(/.{8}/g);
+}
+
+function isValidIp(octets) {
+  return octets.length === 4 && octets.every(octet => octet >= 0 && octet <= 255);
+}
+
+function toFixedBinary(decimal, length) {
+  return decimal.toString(2).padStart(length, '0');
+}
+
+function fromBinary(octet) {
+  return parseInt(octet, 2);
 }
 
 // SETTERS
-function setIpOctets(octets) {
-  ipOctets = octets;
-  ipBinOctets = octets.map(o => o.toString(2).padStart(8, 0));
+function setIp(ip) {
+  ipAddressInput.value = ip;
 }
 
-function setMask(maskLength) {
-  maskBitLength = maskLength;
-  maskBinOctets = '1'.repeat(maskLength).padEnd(32, 0).match(/.{8}/g);
-  maskComplementBinOctets = '0'.repeat(maskLength).padEnd(32, 1).match(/.{8}/g);
+function setIpOctets(decimalOctets) {
+  decimalOctets.forEach((octet, i) => {
+    ipDecimalOctets[i].innerHTML = octet;
+    ipBinaryOctets[i].innerHTML = toFixedBinary(octet, 8);
+  });
+}
+
+function setMask(mask) {
+  maskLengthInput.value = mask;
+  maskLengthSlider.value = mask;
+}
+
+function setMaskOctets(binaryOctets) {
+  binaryOctets.forEach((octet, i) => {
+    maskDecimalOctets[i].innerHTML = fromBinary(octet);
+    maskBinaryOctets[i].innerHTML = octet;
+  });
 }
