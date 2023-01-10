0a5c7a48b1af936f3ea416803279b15cc5b1c03f
Jakub L <47220449+Jakub-L@users.noreply.github.com>
2023-01-10 22:53:44 +0000

    Add a section calculating network prefix

--- js/main.js
+++ js/main.js
@@ -1,23 +1,35 @@
 // GLOBALS
-const INITIAL_BITMASK_LENGTH = 24;
-const INITIAL_IP_ADDRESS = '172.16.254.149';
+let mask = 24;
+let ipAddress = '172.16.254.149';
 
 // ELEMENTS
 const ipAddressInput = document.getElementById('ip_input__field');
 const ipAddressValidator = document.getElementById('ip_input__validator');
-const ipDecimalOctets = document.querySelectorAll('[data-conversion=ip-decimal]');
-const ipBinaryOctets = document.querySelectorAll('[data-conversion=ip-binary]');
+const ipDecimalOctets = document.querySelectorAll('[data-group=ip-decimal]');
+const ipBinaryOctets = document.querySelectorAll('[data-group=ip-binary]');
 
 const maskLengthInput = document.getElementById('mask_input__field');
 const maskLengthSlider = document.getElementById('mask_input__slider');
-const maskDecimalOctets = document.querySelectorAll('[data-conversion=mask-decimal]');
-const maskBinaryOctets = document.querySelectorAll('[data-conversion=mask-binary]');
+const maskDecimalOctets = document.querySelectorAll('[data-group=mask-decimal]');
+const maskBinaryOctets = document.querySelectorAll('[data-group=mask-binary]');
+
+const networkPrefixIpOctets = document.querySelectorAll('[data-group=network-prefix-ip]');
+const networkPrefixMaskOctets = document.querySelectorAll('[data-group=network-prefix-mask]');
+const networkPrefixResultOctets = document.querySelectorAll(
+  '[data-group=network-prefix-result]'
+);
+
+const networkPrefixDecimalOctets = document.querySelectorAll(
+  '[data-group=network-prefix-decimal]'
+);
+const networkPrefixBinaryOctets = document.querySelectorAll(
+  '[data-group=network-prefix-binary]'
+);
 
 // INITIALISATION
-setIp(INITIAL_IP_ADDRESS);
-setMask(INITIAL_BITMASK_LENGTH);
-setMaskOctets(maskToOctets(INITIAL_BITMASK_LENGTH));
-setIpOctets(ipToOctets(INITIAL_IP_ADDRESS));
+setIp(ipAddress);
+setMask(mask);
+setNetworkPrefix(ipAddress, mask);
 
 // EVENT LISTENERS
 maskLengthInput.addEventListener('change', bitmaskLengthChange);
@@ -28,15 +40,14 @@ ipAddressInput.addEventListener('change', ipAddressChange);
 function bitmaskLengthChange({ target }) {
   const clampedValue = Math.max(0, Math.min(32, target.value));
   setMask(clampedValue);
-  setMaskOctets(maskToOctets(clampedValue));
+  setNetworkPrefix(ipAddress, clampedValue);
 }
 
 function ipAddressChange({ target }) {
   const newIp = target.value;
-  const newOctets = ipToOctets(newIp);
-  if (isValidIp(newOctets)) {
+  if (isValidIp(newIp)) {
     setIp(newIp);
-    setIpOctets(newOctets);
+    setNetworkPrefix(newIp, mask);
     ipAddressValidator.classList.add('hidden');
   } else {
     ipAddressValidator.classList.remove('hidden');
@@ -49,14 +60,15 @@ function ipToOctets(ip) {
 }
 
 function maskToOctets(length) {
-  return '1'.repeat(length).padStart(32, '0').match(/.{8}/g);
+  return '1'.repeat(length).padEnd(32, '0').match(/.{8}/g);
 }
 
-function isValidIp(octets) {
+function isValidIp(ip) {
+  const octets = ipToOctets(ip);
   return octets.length === 4 && octets.every(octet => octet >= 0 && octet <= 255);
 }
 
-function toFixedBinary(decimal, length) {
+function toBinary(decimal, length = 8) {
   return decimal.toString(2).padStart(length, '0');
 }
 
@@ -65,25 +77,41 @@ function fromBinary(octet) {
 }
 
 // SETTERS
-function setIp(ip) {
-  ipAddressInput.value = ip;
-}
-
-function setIpOctets(decimalOctets) {
-  decimalOctets.forEach((octet, i) => {
+function setIp(newIp) {
+  ipAddress = newIp;
+  ipAddressInput.value = newIp;
+  ipToOctets(newIp).forEach((octet, i) => {
     ipDecimalOctets[i].innerHTML = octet;
-    ipBinaryOctets[i].innerHTML = toFixedBinary(octet, 8);
+    ipBinaryOctets[i].innerHTML = toBinary(octet);
+    networkPrefixIpOctets[i].innerHTML = toBinary(octet);
   });
 }
 
-function setMask(mask) {
-  maskLengthInput.value = mask;
-  maskLengthSlider.value = mask;
-}
-
-function setMaskOctets(binaryOctets) {
-  binaryOctets.forEach((octet, i) => {
+function setMask(newMask) {
+  mask = newMask;
+  maskLengthInput.value = newMask;
+  maskLengthSlider.value = newMask;
+  maskToOctets(newMask).forEach((octet, i) => {
     maskDecimalOctets[i].innerHTML = fromBinary(octet);
     maskBinaryOctets[i].innerHTML = octet;
+    networkPrefixMaskOctets[i].innerHTML = octet;
+  });
+}
+
+function setNetworkPrefix(newIp, newMask) {
+  const maskBits = maskToOctets(newMask).join('').split('').map(Number);
+  const ipBits = ipToOctets(newIp)
+    .map(o => toBinary(o))
+    .join('')
+    .split('')
+    .map(Number);
+  const result = ipBits
+    .map((iBit, i) => iBit & maskBits[i])
+    .join('')
+    .match(/.{8}/g);
+  result.forEach((octet, i) => {
+    networkPrefixResultOctets[i].innerHTML = octet;
+    networkPrefixBinaryOctets[i].innerHTML = octet;
+    networkPrefixDecimalOctets[i].innerHTML = fromBinary(octet);
   });
 }
